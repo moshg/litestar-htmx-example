ccfd6ddea51b3af3f2d42a26f8f57bf9c758d388
mosh <mosh.ohayo@gmail.com>
2026-01-08 21:59:22 +0900

    feat: add REST endpoint

--- src/calculator/main.py
+++ src/calculator/main.py
@@ -3,6 +3,7 @@ from typing import Literal
 
 from litestar import Litestar, get
 from litestar.contrib.jinja import JinjaTemplateEngine
+from litestar.exceptions import HTTPException
 from litestar.openapi.config import OpenAPIConfig
 from litestar.openapi.plugins import ScalarRenderPlugin
 from litestar.response import Template
@@ -16,37 +17,61 @@ async def index() -> Template:
     return Template(template_name="index.html")
 
 
-@get("/api/calculate")
-async def calculate(
+@get("/calculate", include_in_schema=False)
+async def calculate_htmx(
     lhs: float,
     rhs: float,
     operator: Literal["add", "subtract", "multiply", "divide"],
 ) -> str:
+    try:
+        result = calculate(lhs, rhs, operator)
+        result_str = str(int(result)) if result == int(result) else str(result)
+        return f"""{result_str}
+<div id="error-message" hx-swap-oob="innerHTML"></div>"""
+    except ValueError as e:
+        return f"""<span class="text-red-500">Error</span>
+<div id="error-message" hx-swap-oob="innerHTML">{e}</div>"""
+
+
+# Although not used, this shows how you could also expose a REST endpoint.
+@get("/api/calculate")
+async def calculate_rest(
+    lhs: float,
+    rhs: float,
+    operator: Literal["add", "subtract", "multiply", "divide"],
+) -> float:
+    try:
+        return calculate(lhs, rhs, operator)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e)) from e
+
+
+def calculate(
+    lhs: float,
+    rhs: float,
+    operator: Literal["add", "subtract", "multiply", "divide"],
+) -> float:
     match operator:
         case "add":
-            result = lhs + rhs
+            return lhs + rhs
         case "subtract":
-            result = lhs - rhs
+            return lhs - rhs
         case "multiply":
-            result = lhs * rhs
+            return lhs * rhs
         case "divide":
             if rhs == 0:
-                return """<span class="text-red-500">Error</span>
-<div id="error-message" hx-swap-oob="innerHTML">Zero division error</div>"""
-            result = lhs / rhs
-
-    result_str = str(int(result)) if result == int(result) else str(result)
-
-    return f"""{result_str}
-<div id="error-message" hx-swap-oob="innerHTML"></div>"""
+                raise ValueError("Zero division error")
+            return lhs / rhs
 
 
 app = Litestar(
-    route_handlers=[index, calculate],
+    route_handlers=[index, calculate_htmx, calculate_rest],
     template_config=TemplateConfig(
         directory=TEMPLATES_DIR,
         engine=JinjaTemplateEngine,
     ),
+    # If you want to provide a REST API, you can enable API documentation here by including openapi_config.
+    # API documentation is available at `/schema`
     openapi_config=OpenAPIConfig(
         title="calculator-api",
         description="Calculator API documentation",
